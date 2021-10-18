8dd68bc16ed2a8246db2c5dd52958a671695eb45
bulantech <bulantech@gmail.com>
2021-10-18 17:18:23 +0700

    fill select

--- myapp/task.js
+++ myapp/task.js
@@ -6,14 +6,22 @@ let password = ''
 let intervalMenu
 let timeoutMenu
 let bathText=''
+let litresText=''
 let maintenanceMode=false
 let blink=false
 
 // test user rfid
 const users = [ 
   {tag: '0443770628', name: 'Jone', userId: '1122334455', password:'1234'},
-  {tag: '', name: 'Admin', userId: '123456', password:'4321', maintenance:true},
+  {tag: '', name: 'Admin', userId: '1234', password:'4321', maintenance:true},
 ]
+
+const maintenanceMenu = [
+	'                ',
+	'Exit            ',
+]
+let menuCount=0
+
 
 //======================== lcd
 const LCD = require('raspberrypi-liquid-crystal');
@@ -21,40 +29,50 @@ const LCD = require('raspberrypi-liquid-crystal');
 const lcd = new LCD(1, 0x27, 16, 2);
 
 //======================== rfid
-const InputEvent = require('input-event');
-const input = new InputEvent('/dev/input/event0');
-const keyboard = new InputEvent.Keyboard(input);
+const rfidInit = () => {
+	const fs = require("fs");
+	const path = '/dev/input/event0'
 
-// https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
-const event_codes = [
-  {key: '1', code: 2},
-  {key: '2', code: 3},
-  {key: '3', code: 4},
-  {key: '4', code: 5},
-  {key: '5', code: 6},
-  {key: '6', code: 7},
-  {key: '7', code: 8},
-  {key: '8', code: 9},
-  {key: '9', code: 10},
-  {key: '0', code: 11},
-]
+	if (!fs.existsSync(path)) {
+		console.log(path, 'not found')
+		return 1
+	} 
 
-let tag = ''
+	const InputEvent = require('input-event');
+	const input = new InputEvent(path);
+	const keyboard = new InputEvent.Keyboard(input);
 
-keyboard.on('keypress', ev => {
-  if(ev.code==28) {
-    console.log('tag =', tag)
-    gTag = tag
-    tag = ''
-    if(gState === 'ready') {
-			task('card-id')
-    }
-    return
-  } 
-  const keyCode = event_codes.find(({code}) => code === ev.code )
-  // console.log(ev.code, keyCode.key)
-  tag += keyCode.key
-});
+	// https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
+	const event_codes = [
+	  {key: '1', code: 2},
+	  {key: '2', code: 3},
+	  {key: '3', code: 4},
+	  {key: '4', code: 5},
+	  {key: '5', code: 6},
+	  {key: '6', code: 7},
+	  {key: '7', code: 8},
+	  {key: '8', code: 9},
+	  {key: '9', code: 10},
+	  {key: '0', code: 11},
+	]
+
+	let tag = ''
+
+	keyboard.on('keypress', ev => {
+	  if(ev.code==28) {
+	    console.log('tag =', tag)
+	    gTag = tag
+	    tag = ''
+	    if(gState === 'ready') {
+				task('card-id')
+	    }
+	    return
+	  } 
+	  const keyCode = event_codes.find(({code}) => code === ev.code )
+	  // console.log(ev.code, keyCode.key)
+	  tag += keyCode.key
+	});
+}
 
 //======================== keypad
 //Pi GPIO6  to keypad R1
@@ -106,6 +124,9 @@ let nowKey = null
 //   }
 //   checkKey(2)
 // });
+
+
+//========================================== key event
 
 const checkKey = (col) => {
 	// console.log('col:==>', col, new Date().getTime())	
@@ -215,8 +236,79 @@ const checkKey = (col) => {
 		case 'fill-select':
 			switch(key) {
 				case '1': task('fill-bath-select'); break;
-				case '2': task('fill-lite-select'); break;
-				case '3': task('fill-full'); break;
+				case '2': task('fill-litres-select'); break;
+				case '3': task('fill-full-confirm'); break;
+			}
+		break
+
+		case 'fill-full-confirm':
+			switch(key) {
+				case '*': return task('ready'); break;
+				case '#': return task('fill-full'); break;
+			}
+		break
+
+		case 'fill-bath-select':
+			switch(key) {
+				case '*': 
+					if(bathText.length) bathText = bathText.slice(0, -1); 
+					else return task('ready'); 
+				break;
+				case '#': 
+					if(bathText.length)
+						return task('fill-bath-confirm'); 
+				break;
+				case '1': case '2': case '3': case '4': case '5': 
+				case '6': case '7': case '8': case '9': case '0': 
+					if(bathText.length<4) bathText += key
+				break;
+			}
+
+			lcd.printLineSync(0, 'Amount> '+bathText.padStart(8, ' '));
+			if(!bathText.length) lcd.printLineSync(1, 'EXIT(*)    OK(#)');
+			else lcd.printLineSync(1, 'CLEAR(*)   OK(#)');
+
+			clearTimeout(timeoutMenu)
+			timeoutMenu = setTimeout(()=>{				
+				task('timeout')
+			}, 10*1000)	
+		break
+		case 'fill-bath-confirm':
+			switch(key) {
+				case '*': return task('ready'); break;
+				case '#': return task('fill-bath'); break;
+			}
+		break
+
+		case 'fill-litres-select':
+			switch(key) {
+				case '*': 
+					if(litresText.length) litresText = litresText.slice(0, -1); 
+					else return task('ready'); 
+				break;
+				case '#': 
+					if(litresText.length)
+						return task('fill-litres-confirm'); 
+				break;
+				case '1': case '2': case '3': case '4': case '5': 
+				case '6': case '7': case '8': case '9': case '0': 
+					if(litresText.length<2) litresText += key
+				break;
+			}
+
+			lcd.printLineSync(0, 'Litres> '+litresText.padStart(8, ' '));
+			if(!litresText.length) lcd.printLineSync(1, 'EXIT(*)    OK(#)');
+			else lcd.printLineSync(1, 'CLEAR(*)   OK(#)');
+
+			clearTimeout(timeoutMenu)
+			timeoutMenu = setTimeout(()=>{				
+				task('timeout')
+			}, 10*1000)	
+		break
+		case 'fill-litres-confirm':
+			switch(key) {
+				case '*': return task('ready'); break;
+				case '#': return task('fill-litres'); break;
 			}
 		break
 
@@ -264,7 +356,7 @@ const scanKeyInterval = () => {
 //======================== main task
 
 const task = state => {
-	console.log('task:', state)
+	console.log(new Date().getTime(), 'task:', state)
 	gState = state
 	switch(state) {
 		case 'init':
@@ -273,12 +365,26 @@ const task = state => {
 			// Clear any previously displayed content
 			lcd.clearSync();
 			// Display text multiline
-			lcd.printLineSync(0, 'init...         ');
+			lcd.printLineSync(0, 'Init...         ');
 			lcd.printLineSync(1, '                ');
 
+			const err = rfidInit()
 			scanKeyInterval()
 
-			task('ready')
+			if(err) {
+				lcd.printLineSync(0, 'Init ERROR      ');
+				lcd.printLineSync(1, 'RFID Not Found  ');
+				clearTimeout(timeoutMenu)
+				timeoutMenu = setTimeout(()=>{
+					task('ready')
+				}, 5*1000)
+				return
+			}
+			
+			lcd.printLineSync(0, 'init OK         '); 
+			timeoutMenu = setTimeout(()=>{
+				task('ready')
+			}, 2*1000)
 		break;
 
 		case 'ready':			
@@ -299,7 +405,7 @@ const task = state => {
 				// console.log(d)
 
 				lcd.printLineSync(0, d);
-				lcd.printLineSync(1, '> Keyin or Tap <');
+				lcd.printLineSync(1, ' Key in or Tap  ');
 			}, 1000)
 		break
 
@@ -321,7 +427,7 @@ const task = state => {
 
 			// console.log(users, user, tag)			
 			lcd.printLineSync(0, 'User> '+userId.padStart(10, ' '));
-			lcd.printLineSync(1, 'CLEAR(*)    OK(#)');
+			lcd.printLineSync(1, 'CLEAR(*)   OK(#)');
 
 			clearTimeout(timeoutMenu)
 			timeoutMenu = setTimeout(()=>{
@@ -426,8 +532,8 @@ const task = state => {
 		break;
 
 		case 'maintenance-menu': 
-			lcd.printLineSync(0, '1) Set time     ');
-			lcd.printLineSync(1, '<(3) >(6)  OK(#)');
+			lcd.printLineSync(0, maintenance[menuCount]);
+			lcd.printLineSync(1, 'NEXT(*)    OK(#)');
 
 			clearTimeout(timeoutMenu)
 			timeoutMenu = setTimeout(()=>{
@@ -444,6 +550,14 @@ const task = state => {
 			}, 10*1000)	
 		break;
 
+		case 'fill-full-confirm': 
+			lcd.printLineSync(0, ' Fill to full?  ');
+			lcd.printLineSync(1, 'NO(*)     YES(#)');
+			clearTimeout(timeoutMenu)
+			timeoutMenu = setTimeout(()=>{
+				task('timeout')
+			}, 10*1000)	
+		break;
 		case 'fill-full':
 			lcd.printLineSync(0, '  Fill to full  ');
 			lcd.printLineSync(1, '>>  Process   <<');
@@ -465,30 +579,90 @@ const task = state => {
 				task('fill-complete')
 			}, 10*1000)	
 		break;
+
 		case 'fill-bath-select':
-			lcd.clearSync();
 			bathText = ''
-			intervalMenu = setInterval(() => {
-				lcd.printLineSync(0, 'Enter Bath?     ');
-				lcd.printLineSync(1, bathText.padStart(16, ' '));
-			}, 500)
+			lcd.printLineSync(0, 'Amount> '+bathText.padStart(8, ' '));
+			lcd.printLineSync(1, 'EXIT(*)    OK(#)');
 			clearTimeout(timeoutMenu)
 			timeoutMenu = setTimeout(()=>{				
-				task('ready')
+				task('timeout')
+			}, 10*1000)	
+		break;
+		case 'fill-bath-confirm': 
+			clearInterval(intervalMenu)
+			lcd.printLineSync(0, 'Fill '+bathText.padStart(4, ' ')+' Bath? ');
+			lcd.printLineSync(1, 'NO(*)     YES(#)');
+			clearTimeout(timeoutMenu)
+			timeoutMenu = setTimeout(()=>{
+				task('timeout')
 			}, 10*1000)	
 		break;
 		case 'fill-bath':
-			clearInterval(intervalMenu)
-			lcd.clearSync();
-			lcd.printLineSync(0, 'Refuel '+bathText+' Bath  ');
-			lcd.printLineSync(1, 'Wait a moment...');
+			lcd.printLineSync(0, ' Fill '+bathText.padStart(4, ' ')+' Bath ');
+			lcd.printLineSync(1, '>>  Process   <<');
+
+			intervalMenu = setInterval(() => {
+				if(blink) {
+					blink=false
+					lcd.printLineSync(1, '>>  Process   <<');
+				}
+				else {
+					blink=true
+					lcd.printLineSync(1, ' >> Process  << ');
+				}
+				
+			}, 1000)
+
 			clearTimeout(timeoutMenu)
 			timeoutMenu = setTimeout(()=>{
 				task('fill-complete')
-			}, 5*1000)	
+			}, 10*1000)	
 		break;
+
+		case 'fill-litres-select':
+			bathText = ''
+			lcd.printLineSync(0, 'Litres> '+bathText.padStart(8, ' '));
+			lcd.printLineSync(1, 'EXIT(*)    OK(#)');
+			clearTimeout(timeoutMenu)
+			timeoutMenu = setTimeout(()=>{				
+				task('timeout')
+			}, 10*1000)	
+		break;
+		case 'fill-litres-confirm': 
+			clearInterval(intervalMenu)
+			lcd.printLineSync(0, 'Fill '+litresText.padStart(3, ' ')+' Litres?');
+			lcd.printLineSync(1, 'NO(*)     YES(#)');
+			clearTimeout(timeoutMenu)
+			timeoutMenu = setTimeout(()=>{
+				task('timeout')
+			}, 10*1000)	
+		break;
+		case 'fill-litres':
+			lcd.printLineSync(0, 'Fill '+litresText.padStart(3, ' ')+' Litres ');
+			lcd.printLineSync(1, '>>  Process   <<');
+
+			intervalMenu = setInterval(() => {
+				if(blink) {
+					blink=false
+					lcd.printLineSync(1, '>>  Process   <<');
+				}
+				else {
+					blink=true
+					lcd.printLineSync(1, ' >> Process  << ');
+				}
+				
+			}, 1000)
+
+			clearTimeout(timeoutMenu)
+			timeoutMenu = setTimeout(()=>{
+				task('fill-complete')
+			}, 10*1000)	
+		break;
+
 		case 'fill-complete':
-			lcd.printLineSync(0, 'Refuel complete ');
+			clearInterval(intervalMenu)
+			lcd.printLineSync(0, '      Done      ');
 			lcd.printLineSync(1, '                ');
 			clearTimeout(timeoutMenu)
 			timeoutMenu = setTimeout(()=>{
